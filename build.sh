b87a92bf2e174591b650316c3e3992c8889c1f1c
Kévin <kevinhal.didier@gmail.com>
2023-01-07 19:38:33 +0100

    gitignore commit

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 !/bin/sh
-#suppression du dossier build s'il existe
-#rm -r build/
 #compilation dans le dosser build
 echo compilation
 javac -d ./build src/Main.java src/graph/*.java src/priorityQueue/*.java      
 cp -r src/META-INF build/
 #création du .jar executable
-
+cd build
 echo "création du jar"
-jar cvfm Dijnstra.jar build/META-INF/MANIFEST.MF build/Main.class build/graph/* build/priorityQueue/*
-
+jar cvfm Dijnstra.jar META-INF/MANIFEST.MF Main.class graph/* priorityQueue/*
+mv Dijnstra.jar ../Dijnstra.jar
